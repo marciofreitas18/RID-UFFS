faa979664930156bd3929ac8593ee0e64ccdf3c2
marciofreitas18 <mfr1808@gmail.com>
2026-08-05 22:20:58 -0300

    Update tabela_rid.js

--- tabela_rid.js
+++ tabela_rid.js
@@ -1,1 +1,7 @@
-
+const categoriasRID = [
+  { id: "ensino", nome: "1. ATIVIDADES DE ENSINO" },
+  { id: "pesquisa", nome: "2. ATIVIDADES DE PESQUISA" },
+  { id: "extensao", nome: "3. ATIVIDADES DE EXTENSÃO" },
+  { id: "formacao", nome: "4. ATIVIDADES DE FORMAÇÃO" },
+  { id: "gestao", nome: "5. ATIVIDADES DE ADMINISTRAÇÃO E GESTÃO UNIVERSITÁRIA" }
+];
